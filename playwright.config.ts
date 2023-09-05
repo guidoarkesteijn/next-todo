0f6837035a5f78d9bd7c214b88c83bc020983777
Guido Arkesteijn <guidoarkesteijn@gmail.com>
2023-09-05 23:52:06 +0200

    Only execute playwright on "tests" folder

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { PlaywrightTestConfig } from '@playwright/test'
 const config: PlaywrightTestConfig = {
     use: {
         baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://localhost:3000'
-    }
+    },
+    testDir: "tests"
 }
 export default config
