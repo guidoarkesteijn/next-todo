b14080c0ed09144ffdfa1d520a41a1f5db837813
Guido Arkesteijn <guidoarkesteijn@gmail.com>
2023-09-09 03:07:48 +0200

    Small layout fix items center

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -8,7 +8,7 @@ export default async function Dashboard() {
 
     return (    
         <div className="w-full flex flex-col">
-            <div className="flex flex-row flex-grow justify-between">
+            <div className="flex flex-row flex-grow justify-between items-center">
                 <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tigh md:text-5xl lg:text-6xl">
                     Dashboard
                 </h1>
