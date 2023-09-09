7e6e3d9fbed6dde85dae1fd74c2e1621a494b460
Guido Arkesteijn <guidoarkesteijn@gmail.com>
2023-09-10 01:18:14 +0200

    Added empty check to show No Todos

--- components/server-component/page.tsx
+++ components/server-component/page.tsx
@@ -10,9 +10,11 @@ export default async function TodoList() {
   const supabase = createServerComponentClient<Database>({ cookies })
 
   const { data: todos } = await supabase.from('todos').select().order('title')
-  
+  const empty = todos?.length == 0;
+
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5'>
+      {empty && <h3 className='text-xl'>No Todos</h3>}
       {todos != null && todos.map((value) => (
         <TodoItem
           id={value.id}
