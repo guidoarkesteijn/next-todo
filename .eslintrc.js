6969ab7d9fbbc7a49fb13674354cbcc3768413bd
Guido Arkesteijn <guidoarkesteijn@gmail.com>
2023-09-11 21:29:22 +0200

    Added prettier support

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,14 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'next/core-web-vitals'],
+  plugins: ['@typescript-eslint', "prettier"],
+  extends: ['eslint:recommended', 'next/core-web-vitals', "prettier"],
   globals: {
     React: true,
     Database: true
+  },
+  rules: {
+    "prettier/prettier": "error"
   },
   ignorePatterns: ["components/ui/**", "lib/**"],
 }
